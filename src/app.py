810c1aa10a8fbb4cf206ded992733f03a73d5c51
PudgeKim <zxaq15@gmail.com>
2025-04-06 21:24:52 +0900

    캐릭터별 통계 초안 작성

--- src/app.py
+++ src/app.py
@@ -1,16 +1,19 @@
 import streamlit as st
 from main_page import show_main
+from character_stats_page import show_all_losing_data_by_character
 from utils import add_new_line
 from stats_page import show_today_losers, show_recent_games, show_recent_losers
 
 main_page_str = "순위 저장"
 stats_page_str = "통계"
+character_stats_page_str = "캐릭터별 통계"
 
 page = st.sidebar.radio(
     "페이지를 선택하세요",
     [
         main_page_str,
         stats_page_str,
+        character_stats_page_str,
     ]
 )
 
@@ -24,3 +27,11 @@ elif page == stats_page_str:
     add_new_line()
 
     show_recent_games()
+elif page == character_stats_page_str:
+    with st.form("character_form"):
+        character_name = st.text_input("캐릭터명을 입력하세요")
+        submitted = st.form_submit_button("확인")
+
+    if submitted:
+        show_all_losing_data_by_character(character_name)
+
---
+++ src/character_stats_page.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+
+def show_all_losing_data_by_character(character_name):
+    st.title("꼴찌한 전체 데이터")
