34140f545f8dce473f4e7bcd0eac8c3faa6652f7
PudgeKim <zxaq15@gmail.com>
2025-04-06 14:00:22 +0900

    오늘의 꼴찌 요청 추가

--- src/stats_page.py
+++ src/stats_page.py
@@ -13,6 +13,16 @@ def show_today_losers():
     """, unsafe_allow_html=True)
     st.caption("하루동안 내기를 2판 이상 한 경우에는 꼴찌가 2명 이상 표기됩니다.")
 
+    response = requests.get(create_url("/races/today-losers"))
+    if response.status_code == 200:
+        losers_data = response.json()
+
+        df = pd.DataFrame(losers_data)
+        df = df[["group_uuid", "character_name", "total_rank"]]
+        st.dataframe(df, use_container_width=True, hide_index=True)
+    else:
+        st.error(f"데이터를 가져오지 못했습니다. error_code: {response.status_code}")
+
 
 def show_recent_losers():
     st.title("최근 꼴찌들")
