0468437ff2054abd3c766a75c9a7e5492dd32b5f
kanetla <zxaq15@npixel.co.kr>
2025-04-08 15:59:36 +0900

    꼴찌 데이터 없을 때 예외처리 추가

--- src/character_stats_page.py
+++ src/character_stats_page.py
@@ -11,8 +11,11 @@ def show_loser_history_by_character(character_name):
     response = requests.get(create_url("/races/loser-history"), params={"character_name": character_name})
     if response.status_code == 200:
         loser_history = response.json()
-        df = pd.DataFrame(loser_history)
-        df = df[["created_at", "group_uuid", "character_name", "total_rank"]]
-        st.dataframe(df, use_container_width=True, hide_index=True)
+        if not loser_history:
+            st.write("아직 꼴찌 데이터가 없습니다.")
+        else:
+            df = pd.DataFrame(loser_history)
+            df = df[["created_at", "group_uuid", "character_name", "total_rank"]]
+            st.dataframe(df, use_container_width=True, hide_index=True)
     else:
         st.error(f"데이터를 가져오지 못했습니다. error_code: {response.status_code}")
